a6579721df47303f22a76de0f99a3032c01d0670
Netleader <netleader+github@pretribe.de>
2022-09-12 13:43:00 +0200

    misc

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,58 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <SettingsPane />
+  <div>test</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SettingsPane from "./components/SettingsPane"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SettingsPane
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+@font-face {
+  font-family: "FiraCode";
+  src: url("./assets/FiraCode-VF.woff2") format("woff2 supports variations"),
+  url("./assets/FiraCode-VF.woff2") format("woff2-variations");
+  font-weight: 100 1000;
+}
+@font-face {
+  font-family: "Rajdhani";
+  font-style: normal;
+  src: url("./assets/rajdhani-v15-latin-300.woff2") format("woff2 "),
+  url("./assets/rajdhani-v15-latin-300.woff2") format("woff2");
+  font-weight: 300;
+}
+
+* {
+  box-sizing: border-box;
+}
+html {
+  font-size: 12px;
+}
+body {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  background: #080808;
+  font: 1em/1.375 FiraCode,arial,sans-serif;
+  margin: 0;
+}
+html, body {
+  height: 100%;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  background: #121212;
+  border-radius: 4px;
+  width: 1000px;
+  height: 600px;
 }
 </style>
---
+++ src/components/SettingsPane.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="settings-wrapper">
+    <div class="title">
+      <div class="logo">Rampart</div>
+      <div class="version-wrapper">
+        <div class="block-start"></div>
+        <div class="version">0.1.0 Beta</div>
+        <div class="block"></div>
+      </div>
+    </div>
+    <div>settings</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SettingsPane"
+}
+</script>
+
+<style lang="scss" scoped>
+.title {
+  margin: 0 20px;
+  user-select: none;
+}
+  .logo {
+    color: #ee3500;
+    font: 36px Rajdhani, arial,sans-serif;
+    font-weight: lighter;
+    text-transform: uppercase;
+    line-height: 1;
+  }
+  .version-wrapper {
+    display: flex;
+    flex-direction: row;
+  }
+.block-start {
+  display: flex;
+  flex-grow: 1;
+  background-color: #383838;
+  margin: -4px 4px 4px 2px;
+  max-width: 12px;
+}
+  .block {
+    display: flex;
+    flex-grow: 1;
+    background-color: #383838;
+    margin-top: -4px;
+    margin-bottom: 4px;
+    margin-left: 4px;
+  }
+  .version {
+    text-transform: uppercase;
+    font-size: 10px;
+    color: #404040;
+    margin-top: -6px;
+  }
+  .settings-wrapper {
+    display: flex;
+    border-bottom: 1px solid #202020;
+    padding: 12px;
+  }
+</style>
